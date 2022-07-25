38f75fa06664cb02404dbc7b9354faa060291965
Roman Benin <romaben@bk.ru>
2022-07-25 20:08:40 +0300

    the programm was modified

--- src/Main.java
+++ src/Main.java
@@ -6,14 +6,27 @@ public class Main {
         Scanner console = new Scanner(System.in);
         BmiService service = new BmiService();
 
-        int weight, heightcm;
+        int weight, heightCM;
         double height, bmi;
 
         System.out.print("Enter your height in cm: ");
-        heightcm = console.nextInt();
-        height = (double) heightcm / 100;
+        heightCM = console.nextInt();
+        if (heightCM < 40){
+            while (heightCM < 40) {
+                System.out.print("Your height can not be lower than a newborn kid, write your height again: ");
+                heightCM = console.nextInt();
+            }
+        }
+        height = (double) heightCM / 100;
+
         System.out.print("Enter your weight in kg: ");
         weight = console.nextInt();
+        if (weight < 2) {
+            while (weight < 2) {
+                System.out.print("Your weight can not be lower than a newborn kid, write your weight again: ");
+                weight = console.nextInt();
+            }
+        }
 
         bmi = service.calculate(weight, height);
 
